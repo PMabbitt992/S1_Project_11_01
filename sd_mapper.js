322cb39167ca1bab374640a85d607746ef84f51d
Paige Mabbitt <PMabbitt992@west-mec.org>
2019-02-12 15:24:57 -0700

    Final Commit: Ready for Review
    it didnt include the new javascript in the last commit

--- sd_mapper.js
+++ sd_mapper.js
@@ -9,19 +9,25 @@
    Date: 2.12.19  
 
 */
-
-//Sets thisTime to a time
-var thisTime = new Date("August 14, 2019");
-//sets timeStr to thisTime
-var timeStr = thisTime.toLocaleString();
-// elements with id timestamp are set equal to timeStr
-document.getElementById("timeStamp").innerHTML = timeStr;
-//Determine hours and month using timeStr
-var thisHour = thisTime.getHours();
-var thisMonth = thisTime.getMonth();
-// Determin map number using the month and hours
-var mapNum = (2 * thisMonth + thisHour) % 24;
-// set a variable to a html element for insertion
-var imgStr = "<img src='sd_sky" + mapNum + ".png' />";
-// insert var imgStr after "planisphere" element
-document.getElementById("planisphere").insertAdjacentHTML('afterbegin', imgStr);
+runClock();
+setInterval("runClock()", 1000);
+//Actual clock function
+function runClock() {
+    //Sets thisTime to a time
+    var thisTime = new Date();
+    var dateStr = thisTime.toLocaleDateString();
+    var timeStr = thisTime.toLocaleTimeString();
+    //sets timeStr to thisTime
+    var timeStr = thisTime.toLocaleString();
+    // elements with id timestamp are set equal to timeStr
+    document.getElementById("timeStamp").innerHTML = timeStr;
+    //Determine hours and month using timeStr
+    var thisHour = thisTime.getHours();
+    var thisMonth = thisTime.getMonth();
+    // Determin map number using the month and hours
+    var mapNum = (2 * thisMonth + thisHour) % 24;
+    // set a variable to a html element for insertion
+    var imgStr = "<img src='sd_sky" + mapNum + ".png' />";
+    // insert var imgStr after "planisphere" element
+    document.getElementById("planisphere").insertAdjacentHTML('afterbegin', imgStr);
+}
